6fddf709ffa05d8de6fca8978646520721c7f755
bluephoenixmedia <dennis.gotto@gmail.com>
2023-05-27 15:00:52 -0400

    Initial commit

---
+++ target/scala-2.13/twirl/main/views/html/index.template.scala
@@ -0,0 +1,241 @@
+
+package views.html
+
+import _root_.play.twirl.api.TwirlFeatureImports._
+import _root_.play.twirl.api.TwirlHelperImports._
+import _root_.play.twirl.api.Html
+import _root_.play.twirl.api.JavaScript
+import _root_.play.twirl.api.Txt
+import _root_.play.twirl.api.Xml
+import models._
+import controllers._
+import play.api.i18n._
+import views.html._
+import play.api.templates.PlayMagic._
+import play.api.mvc._
+import play.api.data._
+
+object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply/*1.2*/():play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](format.raw/*2.1*/("""
+"""),_display_(/*3.2*/main("Welcome")/*3.17*/ {_display_(Seq[Any](format.raw/*3.19*/("""
+"""),_display_(/*4.2*/defining(play.core.PlayVersion.current)/*4.41*/ { version =>_display_(Seq[Any](format.raw/*4.54*/("""
+
+"""),format.raw/*6.1*/("""<section id="content">
+  <div class="wrapper doc">
+    <article>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+          <div>
+              <strong>Ethos / Guiding Life Principles</strong><br /><br />
+              <ul>
+                <li>What is the ethos and guiding principles of the organization?</li>
+                <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+              </ul>
+          </div>
+      </div>
+    </article>
+    <article>
+      <div class="flex-container">
+        <div><img src="assets/images/purpose.png" /></div>
+        <div><strong>Aligned Shared Purpose</strong>
+        <br /><br />
+          <ul>
+            <li>High Reliability Access / Insight</li>
+              <li>Process Improvement</li>
+          </ul>
+        </div>
+      </div>
+    </article>
+    <article>
+      <div class="flex-container">
+        <div><img src="assets/images/governance.png" /></div>
+        <div><strong>Governance</strong><br /><br />
+        <ul>
+          <li>xxx</li>
+          <li>xxx</li>
+        </ul>
+      </div>
+    </article>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article20>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article20>
+    <article50>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article50>
+    <article50>
+      <div class="flex-container">
+        <div><img src="assets/images/earth.png" /></div>
+        <div>
+          <strong>Ethos / Guiding Life Principles</strong><br /><br />
+          <ul>
+            <li>What is the ethos and guiding principles of the organization?</li>
+            <li>How does the organization ethos and guiding principles translate for the collective of teams? (E.g. focus may be Teamwork)</li>
+          </ul>
+        </div>
+      </div>
+    </article50>
+     <!--
+    <aside>
+      """),_display_(/*188.8*/commonSidebar()),format.raw/*188.23*/("""
+    """),format.raw/*189.5*/("""</aside>
+    -->
+  </div>
+</section>
+
+""")))}),format.raw/*194.2*/("""
+""")))}))
+      }
+    }
+  }
+
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+
+  def ref: this.type = this
+
+}
+
+
+              /*
+                  -- GENERATED --
+                  SOURCE: app/views/index.scala.html
+                  HASH: c188ed35497ee1f32748ed02f6c8eb37e5e43203
+                  MATRIX: 722->1|818->4|845->6|868->21|907->23|934->25|981->64|1031->77|1059->79|8253->7246|8290->7261|8323->7266|8393->7305
+                  LINES: 21->1|26->2|27->3|27->3|27->3|28->4|28->4|28->4|30->6|212->188|212->188|213->189|218->194
+                  -- GENERATED --
+              */
+          
