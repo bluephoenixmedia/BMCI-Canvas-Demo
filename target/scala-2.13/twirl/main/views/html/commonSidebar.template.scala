33b6ffdd5e2859a269b809b837580e5648dd44d0
bluephoenixmedia <dennis.gotto@gmail.com>
2023-07-24 10:15:50 -0400

    restructuring project to focus on java vsa scala

--- target/scala-2.13/twirl/main/views/html/commonSidebar.template.scala
+++ target/scala-2.13/twirl/main/views/html/commonSidebar.template.scala
@@ -12,8 +12,14 @@ import controllers._
 import play.api.i18n._
 import views.html._
 import play.api.templates.PlayMagic._
-import play.api.mvc._
-import play.api.data._
+import java.lang._
+import java.util._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.api.data.Field
+import play.data._
+import play.core.j.PlayFormsMagicForJava._
+import scala.jdk.CollectionConverters._
 
 object commonSidebar extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
 
@@ -25,20 +31,16 @@ object commonSidebar extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.
 
 Seq[Any](_display_(/*2.2*/defining(play.core.PlayVersion.current)/*2.41*/ { version =>_display_(Seq[Any](format.raw/*2.54*/("""
 """),format.raw/*3.1*/("""<h3>Table of Contents</h3>
-<ul>
-  <li><a href=""""),_display_(/*5.17*/routes/*5.23*/.HomeController.index()),format.raw/*5.46*/("""#Introduction">Welcome</a>
-  <li><a href=""""),_display_(/*6.17*/routes/*6.23*/.HomeController.explore()),format.raw/*6.48*/("""">Play application overview</a>
-  <li><a href=""""),_display_(/*7.17*/routes/*7.23*/.HomeController.tutorial()),format.raw/*7.49*/("""">Implementing Hello World</a>
-</ul>
+
 <h3>Related Resources</h3>
 <ul>
-  <li><a href="https://playframework.com/documentation/"""),_display_(/*11.57*/version),format.raw/*11.64*/("""" target="_blank">Play documentation</a></li>
+  <li><a href="https://playframework.com/documentation/"""),_display_(/*7.57*/version),format.raw/*7.64*/("""" target="_blank">Play documentation</a></li>
   <li><a href="https://github.com/playframework/playframework/discussions" target="_blank">Forum</a></li>
   <li><a href="https://discord.gg/g5s2vtZ4Fa" target="_blank">Play Discord Server</a></li>
   <li><a href="https://stackoverflow.com/questions/tagged/playframework" target="_blank">Stackoverflow</a></li>
   <li><a href="https://www.playframework.com/sponsors" target="_blank">Sponsor Play</a></li>
 </ul>
-""")))}),format.raw/*17.2*/("""
+""")))}),format.raw/*13.2*/("""
 """))
       }
     }
@@ -56,9 +58,9 @@ Seq[Any](_display_(/*2.2*/defining(play.core.PlayVersion.current)/*2.41*/ { vers
               /*
                   -- GENERATED --
                   SOURCE: app/views/commonSidebar.scala.html
-                  HASH: 6e0f53fbacdbdc6075d2c6cd75940a300860e75f
-                  MATRIX: 730->1|826->5|873->44|923->57|950->58|1024->106|1038->112|1081->135|1150->178|1164->184|1209->209|1283->257|1297->263|1343->289|1495->414|1523->421|2008->876
-                  LINES: 21->1|26->2|26->2|26->2|27->3|29->5|29->5|29->5|30->6|30->6|30->6|31->7|31->7|31->7|35->11|35->11|41->17
+                  HASH: 81e794d014bfbde6bc093216f895f959c328eaa6
+                  MATRIX: 908->1|1004->5|1051->44|1101->57|1128->58|1270->174|1297->181|1782->636
+                  LINES: 27->1|32->2|32->2|32->2|33->3|37->7|37->7|43->13
                   -- GENERATED --
               */
           
